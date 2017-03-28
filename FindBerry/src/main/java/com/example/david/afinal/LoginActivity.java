d0817dc553c5f8afa71af1083e0404b2a3f30ac4
David <rdcs1@hotmail.com>
2017-03-28 17:44:38 -0500

    Added splash

--- FindBerry/src/main/java/com/example/david/afinal/LoginActivity.java
+++ FindBerry/src/main/java/com/example/david/afinal/LoginActivity.java
@@ -2,12 +2,14 @@ package com.example.david.afinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 }
--- FindBerry/src/main/java/com/example/david/afinal/SplashActivity.java
+++ FindBerry/src/main/java/com/example/david/afinal/SplashActivity.java
@@ -1,13 +1,30 @@
 package com.example.david.afinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final long SPLASH_DELAY = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent().setClass(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task,SPLASH_DELAY);
     }
 }
